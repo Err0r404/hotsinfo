da9a295a0849e5a2f492753a8f460ffd6f3a1f10
Julien Schmitt <julien.schmitt@imaios.com>
2018-03-27 15:26:17 +0200

    Added detailled information about a player

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlayerController extends Controller {
     
@@ -42,6 +43,100 @@ class PlayerController extends Controller {
      * @return Response
      */
     public function show($id) {
+        // Get the Hero
+        $players = DB::table('players')
+            ->join('participations', 'players.id', '=', 'participations.player_id')
+            ->join('games', 'participations.game_id', '=', 'games.id')
+            ->select(
+                'players.id',
+                'players.battletag',
+                'players.games',
+                DB::raw('ROUND((victories/games)*100,2) AS winrate'),
+                DB::raw('SUM(games.length) AS length')
+            )
+            ->where('players.id', $id)
+            ->groupBy('player_id')
+            ->get();
+    
+        // Format some data
+        foreach ($players as $player) {
+            $player->readable_games  = $this->numbertoHumanReadableFormat($player->games);
+            $player->readable_length = $this->secondsToHumanReadableString($player->length);
+        }
+        
+        $player = $players['0'];
+        
+        $games = DB::table('games')
+            ->join('participations', 'games.id', '=', 'participations.game_id')
+            ->join('participations as p2', 'games.id', '=', 'p2.game_id')
+            ->join('players', 'p2.player_id', '=', 'players.id')
+            ->join('heroes', 'p2.hero_id', '=', 'heroes.id')
+            ->select(
+                'games.id',
+                'games.length',
+                'games.date',
+                'participations.win',
+                'p2.silenced',
+                'p2.team',
+                'players.id as player_id',
+                'players.battletag',
+                'heroes.id as hero_id',
+                'heroes.name as hero_name'
+            )
+            ->where('participations.player_id', $id)
+            ->orderby('games.date', 'desc')
+            ->paginate(100); // 10 players per party so 100 means 10 parties
+    
+        $previousGameId = null;
+        $firstKey = null;
+        
+        foreach ($games as $key => $game) {
+            
+            if($game->id !== $previousGameId){
+                // Create an array to store all players
+                $game->teammates = [];
+                
+                // Remember some vars
+                $previousGameId = $game->id;
+                $firstKey = $key;
+            }
+            else{
+                unset($games[$key]);
+            }
+    
+            // Create a new player using known data
+            $teammate            = new \stdClass();
+            $teammate->player_id = $game->player_id;
+            $teammate->battletag = $game->battletag;
+            $teammate->silenced  = $game->silenced;
+            $teammate->team      = $game->team;
+            $teammate->hero_id   = $game->hero_id;
+            $teammate->hero_slug = str_slug($game->hero_name);
+            $teammate->hero      = $game->hero_name;
+            
+            // Add the player to an array
+            $games[$firstKey]->teammates[] = $teammate;
+            
+            // Purge data
+            unset($game->battletag);
+            unset($game->player_id);
+            unset($game->hero_name);
+            unset($game->hero_id);
+            unset($game->silenced);
+            unset($game->team);
+            
+            // Format some data
+            $game->readable_length = $this->secondsToHumanReadableString($game->length);
+            $game->ago = $this->datetimeToTimeAgo($game->date);
+        }
+        
+        return view(
+            'players.show',
+            [
+                'player' => $player,
+                'games'  => $games,
+            ]
+        );
     
     }
     
---
+++ resources/views/players/show.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.layout')
+
+@section('title')
+    {{ $player->battletag }}
+@stop
+
+@section('container')
+    {{--
+    <div class="row">
+        <div class="col">
+            @php
+                echo "<pre>";
+                print_r($enemies);
+                echo "</pre>";
+            @endphp
+        </div>
+    </div>
+    --}}
+
+    <div class="row mb-5">
+        <div class="col-2">
+            <img class="img-fluid w-100" src="//via.placeholder.com/150x150" alt="Portrait of {{ $player->battletag }} from Heroes Of The Storm">
+        </div>
+
+        <div class="col-10">
+            <h1>{{ $player->battletag }}</h1>
+            <p class="lead mb-2">{{ $player->readable_games }} games played</p>
+            <p class="lead mb-2 @if($player->winrate >= 50) text-success @else text-danger @endif">{{ $player->winrate }}% of winrate</p>
+            <p class="lead mb-0">{{ $player->readable_length }} played</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="table-responsive">
+                <table id="games" class="table table-sm">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>Result</th>
+                        <th>Length</th>
+                        <th>Date</th>
+                        <th colspan="2">Players</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($games as $game)
+                        <tr>
+                            <td>
+                                @if($game->win)
+                                    <span class="badge badge-success">Victory</span>
+                                @else
+                                    <span class="badge badge-danger">Defeat</span>
+                                @endif
+                            </td>
+                            <td>
+                                {{ $game->readable_length }}</td>
+                            <td>
+                                <span data-container="body" data-toggle="tooltip" data-placement="top" title="{{ $game->date }}">
+                                    {{ $game->ago }}
+                                </span>
+                            </td>
+                            <td>
+                                <ul class="list-unstyled mb-0">
+                                    @foreach($game->teammates as $teammate)
+                                        @if($teammate->team === 0)
+                                            <li>
+                                                <span data-container="body" data-toggle="tooltip" data-placement="top" title="{{ $teammate->hero }}">
+                                                    <img class="rounded-circle img-fluid mr-1" src="{{ URL::asset('/images/heroes/'.$teammate->hero_slug.'.jpg') }}" alt="Portrait of {{ $teammate->hero }} from Heroes Of The Storm" style="max-width: 25px">
+                                                </span>
+
+                                                @if($teammate->silenced == 1)
+                                                    <i class="fa ion-android-microphone-off" data-container="body" data-toggle="tooltip" data-placement="top" title="Silenced"></i>
+                                                @endif
+
+                                                <a href="{{ url('players/'.$teammate->player_id) }}" class="@if($teammate->player_id == $player->id) text-warning @else text-dark @endif">
+                                                    {{ $teammate->battletag }}
+                                                </a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </td>
+                            <td>
+                                <ul class="list-unstyled mb-0">
+                                    @foreach($game->teammates as $teammate)
+                                        @if($teammate->team === 1)
+                                            <li>
+                                                <span data-container="body" data-toggle="tooltip" data-placement="top" title="{{ $teammate->hero }}">
+                                                    <img class="rounded-circle img-fluid mr-1" src="{{ URL::asset('/images/heroes/'.$teammate->hero_slug.'.jpg') }}" alt="Portrait of {{ $teammate->hero }} from Heroes Of The Storm" style="max-width: 25px">
+                                                </span>
+
+                                                @if($teammate->silenced == 1)
+                                                    <i class="fa ion-android-microphone-off" data-container="body" data-toggle="tooltip" data-placement="top" title="Silenced"></i>
+                                                @endif
+
+                                                <a href="{{ url('players/'.$teammate->player_id) }}" class="@if($teammate->player_id == $player->id) text-warning @else text-dark @endif">
+                                                    {{ $teammate->battletag }}
+                                                </a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <nav aria-label="Games navigation">
+                {{ $games->links() }}
+            </nav>
+        </div>
+    </div>
+@endsection
